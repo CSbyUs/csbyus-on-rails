25870f0d680777f2c3538b93492daaed7ef3da41
CandyZQ <candyzhouziqing@hotmail.com>
2019-03-31 13:36:31 -0400

    more tests and model constrainst

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181016150422) do
+ActiveRecord::Schema.define(version: 20190331172136) do
 
   create_table "comments", force: :cascade do |t|
     t.string "author"
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
   end
 
 end
