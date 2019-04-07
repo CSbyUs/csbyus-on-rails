266631906ea6dc5f58acb6dfdbdf845321199cfa
CandyZQ <candyzhouziqing@hotmail.com>
2019-04-07 13:47:46 -0400

    one controller test not pass

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -22,7 +22,7 @@ class UserTest < ActiveSupport::TestCase
 
    test "email validation should accept valid addresses" do
      valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                            first.last@foo.jp alice+bob@baz.cn]
+                            first.last@foo.jp alice+bob@baz.cn USER@example.com]
      valid_addresses.each do |valid_address|
        @user.email = valid_address
        assert @user.valid?, "#{valid_address.inspect} should be valid"
@@ -35,4 +35,11 @@ class UserTest < ActiveSupport::TestCase
        @user.save
        assert_not duplicate_user.valid?
    end
+
+    test "email addresses should be saved as lower-case" do
+       mixed_case_email = "Foo@ExAMPle.CoM"
+       @user.email = mixed_case_email
+       @user.save
+       assert_equal mixed_case_email.downcase, @user.reload.email
+     end
 end
