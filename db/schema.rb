1658abab87274183e5ef0f791ae97159e3cf2eb9
CandyZQ <candyzhouziqing@hotmail.com>
2019-04-12 19:45:05 -0400

    reset user model. Everything works fine now

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190331172136) do
+ActiveRecord::Schema.define(version: 20190412231156) do
 
   create_table "comments", force: :cascade do |t|
     t.string "author"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20190331172136) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
