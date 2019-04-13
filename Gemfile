1d1b75a06922da51e6c782063a4ed18dd8786f54
CandyZQ <candyzhouziqing@hotmail.com>
2019-04-13 00:16:54 -0400

    basic user model created

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
+
 source 'https://rubygems.org'
 
 ruby "2.4.1"
@@ -58,3 +59,6 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "react-rails"
+
+# For hash passwords
+gem 'bcrypt'
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(username: "Example User", email: "user@example.com", password: "123456")
+    @user = User.new(username: "Example User", email: "user@example.com", password: "123456", password_confirmation: "123456")
   end
 
   test "should be valid" do
@@ -42,4 +42,14 @@ class UserTest < ActiveSupport::TestCase
        @user.save
        assert_equal mixed_case_email.downcase, @user.reload.email
      end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 end
