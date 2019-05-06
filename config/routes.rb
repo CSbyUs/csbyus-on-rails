201aed00cfd0f22ac3afa4f0102fed1f5909ff76
CandyZQ <candyzhouziqing@hotmail.com>
2019-05-05 22:26:12 -0400

    commit for branch changing

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   resources :users
   root 'pages#index'
 
-  get  '/signup',  to: 'users#new'
-
   namespace :api do
     namespace :v1 do
       resources :comments, only: [:index, :create, :destroy, :update]
