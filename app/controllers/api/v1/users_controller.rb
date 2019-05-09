48087b4236b82c0158fb90e3770b99763acca262
Tanner Johnson <tlj21@duke.edu>
2019-05-09 20:33:34 +0200

    expanded user_controller to be CRUD and created db seed

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,37 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
+
+  def index
+    render json: User.all
+  end
+
+  def create
+    user = User.create(user_params)
+    render json: user
+  end
+
+  def destroy
+    User.destroy(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update_attributes(user_params)
+    render json: user
+  end
 
   def show
-    @user = User.find(params[:id])
+    user = User.find(params[:id])
+    render json: user
   end
 
   def new
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :name, :email, :password)
+  end
+
+
 end
